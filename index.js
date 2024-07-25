97003970965a8f3831e1cf7dc4d9ce11190de0ff
Atith794 <atith91098@gmail.com>
2024-07-25 21:35:39 +0530

    commit

--- index.js
+++ index.js
@@ -129,6 +129,7 @@ wss.on('connection', (ws, req) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
